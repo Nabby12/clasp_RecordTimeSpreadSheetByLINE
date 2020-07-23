46f8cb06c9ac3d178a26192de1a4e5a043a7b1c5
Nabby12 <Nabby12@users.noreply.github.com>
2020-07-23 19:56:43 +0900

    modified/authentication-for-line

--- src/main.ts
+++ src/main.ts
@@ -10,33 +10,35 @@ function doPost(e: string) {
 
     let userId: string = event.source.userId;
 
-    if (event.type == 'message') {
-        let userMessage: string = event.message.text.replace(/　/g, ' ').trim();
-
-        let replyMessageToLINE: string = userMessage;
-
-        if (replyMessageToLINE === ''){
-            replyMessageToLINE = 'invalid Text';
-        }
-
-        // send to LINE
-        const LINE_HTTPREQUEST_REPLY: string = 'https://api.line.me/v2/bot/message/reply';
-        UrlFetchApp.fetch(LINE_HTTPREQUEST_REPLY, {
-          'headers': {
-            'Content-Type': 'application/json; charset=UTF-8',
-            'Authorization': 'Bearer ' +LINE_CHANNEL_ACCESSTOKEN,
-          },
-          'method': 'post',
-          'payload': JSON.stringify({
-            'replyToken': replyToken,
-            'messages': [{
-              'type': 'text',
-              'text': replyMessageToLINE,
-            }],
-          }),
-        });
-        
-        return ContentService.createTextOutput(
-        JSON.stringify({'content': 'post ok'})
-        ).setMimeType(ContentService.MimeType.JSON);
+    if (event.type !== 'message') {
+        return;
     }
+    let userMessage: string = event.message.text.replace(/　/g, ' ').trim();
+
+    let replyMessageToLINE: string = userMessage;
+
+    if (replyMessageToLINE === ''){
+        replyMessageToLINE = 'invalid Text';
+    }
+
+    // send to LINE
+    const LINE_HTTPREQUEST_REPLY: string = 'https://api.line.me/v2/bot/message/reply';
+    UrlFetchApp.fetch(LINE_HTTPREQUEST_REPLY, {
+        'headers': {
+        'Content-Type': 'application/json; charset=UTF-8',
+        'Authorization': 'Bearer ' + LINE_CHANNEL_ACCESSTOKEN,
+        },
+        'method': 'post',
+        'payload': JSON.stringify({
+        'replyToken': replyToken,
+        'messages': [{
+            'type': 'text',
+            'text': replyMessageToLINE,
+        }],
+        }),
+    });
+        
+    // return ContentService.createTextOutput(
+    // JSON.stringify({'content': 'post ok'})
+    // ).setMimeType(ContentService.MimeType.JSON);
+}
